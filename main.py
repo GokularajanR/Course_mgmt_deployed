df9777cc995675b51538044ceace4f9dbe64a78c
gulugulu2042 <gokul26.09.2004@gmail.com>
2025-05-04 23:37:27 +0530

    fix uvicorn run command

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ main_api = FastAPI()
 main_api.include_router(app)
 
 if __name__ == "__main__":
-    uvicorn.run("main:main_api", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:main_api")
